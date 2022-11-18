a50327b5bdcacfacb285269ae035adac8ffc5d10
Wikus Schalkwyk <wikus@hyvemobile.co.za>
2022-11-18 08:44:19 +0200

    adding some thoughts - component: Navigation - component: Catalog

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,33 @@ import type { Component } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
+import Navigation from "./components/Navigation";
+import Catalog from "./components/Catalog";
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div>
+      <Navigation/>
+        <div class={'flex text-center '}>
+        <ul class={'mx-auto'}>
+            <li>Catalog</li>
+            <li>User Account + management</li>
+            <li>Cart</li>
+            <li>Checkout</li>
+            <li>Product API</li>
+            <li>Admin: Catalog Management [categories/products/export/import]</li>
+            <li>Admin: Order Management [orders/invoices/payments/export]</li>
+            <li>Admin: Customer Management [customers/export/import]</li>
+            <li>Queue - Kafka</li>
+            <li>Observability - Prometheus</li>
+            <li>Graphing - Grafana</li>
+            <li>Testing - FE - Playwrite</li>
+            <li>Testing - BE - ???</li>
+            <li>DB ?  Mongo? [something in docker/podman]?</li>
+
+        </ul>
+        </div>
+        <Catalog/>
     </div>
   );
 };
